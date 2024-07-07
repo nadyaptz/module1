55d7a2a2b79e37c7d56047106f1ebb23517b12da
nadyaptz <nadya.ptz@gmail.com>
2024-07-07 20:39:29 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,7 @@
+example="экзистенциальный"
+print (example[0])
+print (example[-1])
+string_length_half=int(len(example)/2)
+print (example[string_length_half:])
+print (example[::-1])
+print (example[1::2])
---
+++ homework2.py
@@ -0,0 +1,6 @@
+number_of_homeworks_done=12
+hours_spent=1.5
+course_title='Python'
+time_for_one_task=hours_spent/number_of_homeworks_done
+
+print ('Курс:',course_title+",",'всего задач:',str(number_of_homeworks_done)+",","затрачено времени:",str(hours_spent)+",","среднее время выполнения:",time_for_one_task,"часов")
