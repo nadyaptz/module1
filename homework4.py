b4c04ca9c32d41f5755328f39feef74855e501e3
nadyaptz <nadya.ptz@gmail.com>
2024-07-08 15:54:23 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string=input("Введите произвольную строку: ")
+print ("длина строки:", len(my_string), "символов")
+# print(type(my_string))
+print (my_string.upper())
+print (my_string.lower())
+print (my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
